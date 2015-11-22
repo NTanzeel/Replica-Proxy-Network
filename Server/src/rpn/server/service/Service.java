2346fae7a28fa5f5310495fb7e0722f08dcd5805
ishegambe <ishegambe@yahoo.co.uk>
2015-11-22 11:39:56 +0000

    Updated Service Object
    Removed connections to gateway as this will be handled by the Proxy
    object

--- Server/src/rpn/server/service/Service.java
+++ Server/src/rpn/server/service/Service.java
@@ -11,16 +11,8 @@ import java.util.Scanner;
 
 public class Service {
     private HashMap<String, Stock> stocks = new HashMap<String, Stock>();
-    private String ip;
-    private int port;
-    private Socket socket;
-    private DataInputStream in;
-    private DataOutputStream out;
 
-    public Service(String ip, int port){
-        //Specify IP address and port number of Gateway
-        this.ip = ip;
-        this.port = port;
+    public Service(){
         initaliseMarket();
     }
 
@@ -36,32 +28,27 @@ public class Service {
     }
 
     //Send current stock market status to the client as a byte stream
-    public void sendStocks(){
-        try{
-            //convert map to stream of format name;quantity;price
-            for(Map.Entry<String, Stock> entry : stocks.entrySet()){
-                String output = entry.getKey() + "," + entry.getValue().getQuantity() + "," + entry.getValue().getPrice() + ";";
-                out.writeBytes(output);
-                out.flush();
-            }
+    public String sendStocks(){
+        String stockString = "";
+
+        //Convert map to stream of format name;quantity;price
+        for(Map.Entry<String, Stock> entry : stocks.entrySet()){
+            stockString += entry.getKey() + "," + entry.getValue().getQuantity() + "," + entry.getValue().getPrice() + ";";
         }
-        catch(Exception e){
-            System.out.println("Error sending available stocks");
-        }
+
+        return stockString;
     }
 
-    public void processRequest(int request, String stock, int quantity){
-        //Request to buy stock
-        if(request == 0){
+    public void processRequest(int code, String stock, int quantity){
+        int currentQuantity = stocks.get(stock).getQuantity();
 
+        //Request to buy stock
+        if(code == 0){
+            stocks.get(stock).setQuantity(currentQuantity - quantity);
         }
         //Request to sell stock
         else{
-
+            stocks.get(stock).setQuantity(currentQuantity + quantity);
         }
-    }
-
-    public static void main(String args[]) {
-        Service client = new Service("192.168.0.23", 43590);
     }
 }
