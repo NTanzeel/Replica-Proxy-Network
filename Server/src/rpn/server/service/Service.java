ca7eaad3af6bd72d8e06b1b87b950b62240f7d66
ishegambe <ishegambe@yahoo.co.uk>
2015-11-22 12:35:43 +0000

    Introduced modularity to Service Object

--- Server/src/rpn/server/service/Service.java
+++ Server/src/rpn/server/service/Service.java
@@ -1,10 +1,11 @@
 package rpn.server.service;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.net.Socket;
 
 public class Service {
     private HashMap<String, Stock> stocks = new HashMap<String, Stock>();
+    private Socket socket;
 
     public Service(){
         initialiseMarket();
@@ -26,28 +27,56 @@ public class Service {
         String stockString = "";
 
         //Convert map to stream of format name;quantity;price
-        for(Map.Entry<String, Stock> entry : stocks.entrySet()){
+        for(HashMap.Entry<String, Stock> entry : stocks.entrySet()){
             stockString += entry.getKey() + "," + entry.getValue().getQuantity() + "," + entry.getValue().getPrice() + ";";
         }
 
         return stockString;
     }
 
+    //Process the request received from the client
     public int processRequest(int code, String stock, int quantity){
-        int currentQuantity = stocks.get(stock).getQuantity();
+        int status;
 
-        if(quantity > currentQuantity){
-            return 0;
-        } else {
-            //Request to buy stock
-            if (code == 0) {
-                stocks.get(stock).setQuantity(currentQuantity - quantity);
-            }
-            //Request to sell stock
-            else {
-                stocks.get(stock).setQuantity(currentQuantity + quantity);
-            }
-            return  1;
+        //Depending on request code (buy or sell), update stock amount
+        switch(code){
+            case 0 :
+                status = buyStock(stock, quantity);
+                break;
+            case 1:
+                status = sellStock(stock, quantity);
+                break;
+            default:
+                System.out.println("Invalid option");
+                //Error code 3 represents an invalid option
+                status = 3;
+                break;
         }
+
+        return status;
+    }
+
+    public boolean checkQuantity(int buyAmount, int availableStocks){
+        if( buyAmount > availableStocks) { return false; }
+        else { return true; }
+    }
+
+    public int buyStock(String stock, int quantity){
+        int availableStocks = stocks.get(stock).getQuantity();
+
+        if(checkQuantity(quantity, availableStocks)) {
+            stocks.get(stock).setQuantity(availableStocks - quantity);
+
+            return 1;
+        }else{
+            //Error code 2 represents trying to buy too many stocks
+            return 2;
+        }
+    }
+
+    public int sellStock(String stock, int quantity){
+        stocks.get(stock).setQuantity(stocks.get(stock).getQuantity() - quantity);
+
+        return 1;
     }
 }
